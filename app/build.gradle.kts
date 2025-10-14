4628cfc72193a0d6d120a88350ebb6e53ebfd618
null-proto <209804848+null-proto@users.noreply.github.com>
2025-10-14 23:33:45 +0530

    adds Room orm

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,6 +105,9 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
 
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)

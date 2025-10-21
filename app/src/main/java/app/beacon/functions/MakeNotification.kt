cd733aa4bbde6f429fabe23544e0ef3f5acce7fb
null-proto <209804848+null-proto@users.noreply.github.com>
2025-10-21 21:20:55 +0530

    update makenotifi

--- app/src/main/java/app/beacon/functions/MakeNotification.kt
+++ app/src/main/java/app/beacon/functions/MakeNotification.kt
@@ -1,8 +1,17 @@
 package app.beacon.functions
 
+import android.Manifest
+import android.app.NotificationManager
 import android.content.Context
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.annotation.RequiresPermission
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import app.beacon.core.database.schema.Pop
 import app.beacon.functions.MakeNotification.Args
+import app.beacon.state.Globals
 
 class MakeNotification : Task<Args> {
     data class Args(
@@ -12,6 +21,16 @@ class MakeNotification : Task<Args> {
     )
 
     override fun run(context: Context, arg: Args) {
-        
+        val notify = NotificationCompat.Builder(context, arg.whois.name)
+            .setContentTitle(arg.title)
+            .setContentText(arg.message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        if (ContextCompat.checkSelfPermission(context , Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            NotificationManagerCompat.from(context).notify(arg.message.hashCode() , notify)
+        } else {
+            Log.e("MakeNotification" , "permission denied : ${arg.whois.name} - ${arg.title} - ${arg.message}")
+        }
     }
 }

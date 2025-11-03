e9f914076618c82a987cc5116fefea364fae79e7
null-proto <209804848+null-proto@users.noreply.github.com>
2025-11-03 21:05:57 +0530

    adds debug options

--- app/src/main/java/app/beacon/ui/fragments/settings/Debug.kt
+++ app/src/main/java/app/beacon/ui/fragments/settings/Debug.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Pause
 import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material.icons.rounded.RestartAlt
 import androidx.compose.runtime.Composable
@@ -46,6 +47,16 @@ import java.util.concurrent.TimeUnit
                 )
 
                 Toast.makeText(context, "StartDaemon is polled", Toast.LENGTH_SHORT).show()
+            }
+        )
+
+        Item(
+            name = "Kill Daemon",
+            description = "Stop daemon if running",
+            icon = Icons.Rounded.Pause,
+            onClick = {
+                context.stopService(daemon)
+                Toast.makeText(context, "StartDaemon stopped", Toast.LENGTH_SHORT).show()
             }
         )
 
